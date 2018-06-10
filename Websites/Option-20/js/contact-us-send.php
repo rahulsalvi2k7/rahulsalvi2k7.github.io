47d494fcdbe128bd4956ee853ea2ff0b66d6d1d8
Rahul Salvi <Rahul.Salvi@manheim.co.uk>
2018-06-10 21:58:54 +0100

    added email functionality

--- Websites/Option-20/js/contact-us-send.php
+++ Websites/Option-20/js/contact-us-send.php
@@ -1,22 +1,26 @@
-if(isset($_POST['Submit'])) {
-    // do code
-    // submitted to server through form
-    // build the email message by using
-    // info received by the HTML form
-    $msg =  'Name: ' .$_POST['reply_form_name'] ."\n"
-	        'Email: ' .$_POST['reply_form_email'] ."\n"
-	        'Comment: ' ."\n" .$_POST['reply_form_message'];
+<?php 
 
-    // send email using PHP's built in 
-    // command, mail()
-    mail('contactus@thehirepoint.com',  'Message on Website ' .$_POST['reply_form_name'] , $msg);
+    if(isset($_POST['submit'])) {
+        // do code
+        // submitted to server through form
+        // build the email message by using
+        // info received by the HTML form
+        $msg =  'Name: ' .$_POST['reply_form_name'] ."\n"
+                'Email: ' .$_POST['reply_form_email'] ."\n"
+                'Comment: ' ."\n" .$_POST['reply_form_message'];
 
-    // redirect to the thank you page
-    header('location: index.html');
+        // send email using PHP's built in 
+        // command, mail()        
+        mail('contactus@thehirepoint.com',  'Message on Website ' .$_POST['reply_form_name'] , $msg);
 
-    // exit this script - just to make sure nothing else gets run
-    exit(0);
-} else {
-  // do other code block
-  header('Location: index.html#contactUs');
-}
+        // redirect to the thank you page
+        header('location: index.html');
+
+        // exit this script - just to make sure nothing else gets run
+        exit(0);
+    } else {
+    // do other code block
+    header('Location: index.html#contactUs');
+    }
+
+?>
